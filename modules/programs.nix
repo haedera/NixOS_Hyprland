0c89a136cdf9f26a8736abe3503092614fff5e97
haedera <haderue@pm.me>
2026-02-21 18:05:20 +0100

    Proton-Mail fix?

--- modules/programs.nix
+++ modules/programs.nix
@@ -13,6 +13,20 @@
 	enable = true;
  };
 
+ # Proton-Mail
+ environment.systemPackages = [ pkgs.protonmail-desktop ];
+ xdg.mime.enable = true;
+ xdg.desktopEntries."proton-mail" = {
+   name = "Proton Mail";
+   genericName = "Mail Client";
+   comment = "Proton Mail Desktop";
+   exec = "proton-mail %U";
+   icon = "proton-mail";
+   terminal = false;
+   categories = [ "Network" "Email" ];
+   mimeType = [ "x-scheme-handler/mailto" ];
+ };
+
  # Virtualisation
  virtualisation.libvirtd.enable = true;
  programs.virt-manager.enable = true;
